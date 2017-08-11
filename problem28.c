fb1622c70aa83e17cd69d0298266537214661e45
Ionut Lupu <lupu.ionut@protonmail.ch>
2017-08-11 18:54:48 +0200

    add description problem28

--- problem28.c
+++ problem28.c
@@ -1,3 +1,20 @@
+/*
+Starting with the number 1 and moving to the right in a clockwise direction
+a 5 by 5 spiral is formed as follows:
+
+21 22 23 24 25
+20  7  8  9 10
+19  6  1  2 11
+18  5  4  3 12
+17 16 15 14 13
+
+It can be verified that the sum of the numbers on the diagonals is 101.
+
+What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed
+in the same way?
+*/
+
+
 #include <stdio.h>
 int * cycle(int start, int k);
 
@@ -19,7 +36,7 @@ void main()
         lambda = lambda + 2;
     }
 
-    printf("\%i",total);
+    printf("%i",total);
 }
 
 int * cycle(int start, int k)
