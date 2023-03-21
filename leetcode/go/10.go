c67b94f9e8bf42d11412cbe47e0cde1d9c379259
Ionut Lupu <lupu.ionut@protonmail.ch>
2023-03-21 17:04:08 +0200

    Solve 10. Regular Expression Matching

--- leetcode/go/10.go
+++ leetcode/go/10.go
@@ -8,14 +8,19 @@ func IsMatch(s string, p string) bool {
         return false
     }
 
-    fm := false
+    match := false
+
+    // first check if we match the current char
     if len(s) > 0 && (p[0] == s[0] || p[0] == '.') {
-        fm = true
+        match = true
     }
 
+    // if next char of pattern is * we have 2 options
+    // either we dont use the * and we compare s with p[2:]
+    // or we use the * and we compare s[1:] with p
     if len(p) >= 2 && p[1] == '*' {
-        return IsMatch(s, p[2:]) || (fm && IsMatch(s[1:],p))
+        return IsMatch(s, p[2:]) || (match && IsMatch(s[1:],p))
     } else{
-        return fm && IsMatch(s[1:], p[1:])
+        return match && IsMatch(s[1:], p[1:])
     }
 }
