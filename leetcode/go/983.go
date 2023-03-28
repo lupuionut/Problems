9ba0918aa0b245b2b2684dfc6c2e86356a593a0a
Ionut Lupu <lupu.ionut@protonmail.ch>
2023-03-28 09:49:42 +0300

    Solve 983. Minimum Cost For Tickets

--- leetcode/go/983.go
+++ leetcode/go/983.go
@@ -1,6 +1,18 @@
 package leetcode
 
-//import "fmt"
+/*
+    983. Minimum Cost For Tickets
+
+    Make a slice (d) with len 365 and for each day when we don't need ticket, use
+    value 0.
+    So, when we iterate, we move forward when we don't need a ticket.
+    On the day we need a ticket, get the min value from
+        - 1 day ticket + jump 1 day
+        - 7 day ticket + jump 7 days
+        - 30 day ticket + jump 30 days
+    Use also a slice (m) to keep track of each day cost so we don't have to
+    calculate each time we hit that day.
+*/
 
 func MincostTickets(days []int, costs []int) int {
 
