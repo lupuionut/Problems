baff1da4d85f1676c7562484f28241fa80b84f13
Ionut Lupu <lupu.ionut@protonmail.ch>
2023-04-11 09:36:15 +0300

    Solve 2390. Removing Stars From a String

---
+++ leetcode/go/2390.go
@@ -0,0 +1,28 @@
+package leetcode
+
+/*
+    2390. Removing Stars From a String
+    ----------------------------------
+    total - keep track of total stars, each time we encounter a star total will
+    increase with 2, since we need to move back 2 positions (his position and deleted
+    char position).
+*/
+func removeStars(s string) string {
+
+    total := 0
+    sb := []byte(s)
+
+    for i := 0; i < len(sb); i++ {
+        if sb[i] != '*' {
+            if total == 0 {
+                continue
+            }
+            j := i - total
+            sb[j] = sb[i]
+        } else {
+            total += 2
+        }
+    }
+    end := len(s) - total
+    return string(sb[0:end])
+}
