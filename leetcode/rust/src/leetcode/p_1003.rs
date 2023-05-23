f7fbb66aebba806f5264c02e30dae7e9a26bb0e1
Ionut Lupu <lupu.ionut@protonmail.ch>
2023-05-23 10:24:07 +0300

    Solve 1003. Check If Word Is Valid After Substitutions

---
+++ leetcode/rust/src/leetcode/p_1003.rs
@@ -0,0 +1,30 @@
+// 1003. Check If Word Is Valid After Substitutions
+
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        let mut left = 0;
+        let mut right = 0;
+        let mut stack: Vec<char> = Vec::new();
+
+        if s.len() % 3 != 0 {
+            return false;
+        }
+
+        for c in s.chars() {
+            stack.push(c);
+            if stack.len() > 2 {
+                let l = stack.len() - 3;
+                let r = stack.len();
+                let p = &stack[l..r].iter().collect::<String>();
+                if p == "abc" {
+                    stack.drain(l..r);
+                }
+            }
+        }
+
+        if stack.len() == 0 {
+            return true;
+        }
+        false
+    }
+}
