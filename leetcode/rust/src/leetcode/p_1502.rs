ed086faf4d556f61a6d8ae2728506e580c8b54f9
Ionut Lupu <lupu.ionut@protonmail.ch>
2023-06-06 10:19:21 +0300

    Solve 1502. Can Make Arithmetic Progression From Sequence

--- leetcode/rust/src/leetcode/p_1502.rs
+++ leetcode/rust/src/leetcode/p_1502.rs
@@ -5,10 +5,10 @@ impl Solution {
     pub fn can_make_arithmetic_progression(arr: Vec<i32>) -> bool {
         let mut arr = arr;
         arr.sort();
-        let delta = (&arr[0] - &arr[1]).abs();
+        let delta = (arr[0] - arr[1]).abs();
 
         for i in 1..arr.len() {
-            let current = (&arr[i] - &arr[i - 1]).abs();
+            let current = (arr[i] - arr[i - 1]).abs();
             if current != delta {
                 return false;
             }
