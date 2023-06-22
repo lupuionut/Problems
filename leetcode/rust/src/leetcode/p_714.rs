df674aebb34670c44dd82ad1b9373dbf10911ef3
Ionut Lupu <lupu.ionut@protonmail.ch>
2023-06-22 11:20:35 +0300

    Solve 714. Best Time to Buy and Sell Stock with Transaction Fee

---
+++ leetcode/rust/src/leetcode/p_714.rs
@@ -0,0 +1,37 @@
+// 714. Best Time to Buy and Sell Stock with Transaction Fee
+// ---------------------------------------------------------
+
+impl Solution {
+    pub fn max_profit(prices: Vec<i32>, fee: i32) -> i32 {
+        let mut cache: Vec<Vec<i32>> = vec![vec![-1; 2]; prices.len()];
+
+        fn dp(
+            i: usize,
+            state: usize,
+            prices: &Vec<i32>,
+            fee: i32,
+            cache: &mut Vec<Vec<i32>>,
+        ) -> i32 {
+            if i == prices.len() {
+                return 0;
+            }
+            if cache[i][state] != -1 {
+                return cache[i][state];
+            }
+
+            if state == 0 {
+                let buy = -prices[i] + dp(i + 1, 1, prices, fee, cache);
+                let skip = dp(i + 1, 0, prices, fee, cache);
+                cache[i][state] = buy.max(skip);
+            } else {
+                let sell = prices[i] - fee + dp(i + 1, 0, prices, fee, cache);
+                let skip = dp(i + 1, 1, prices, fee, cache);
+                cache[i][state] = sell.max(skip);
+            }
+
+            cache[i][state]
+        }
+
+        dp(0, 0, &prices, fee, &mut cache)
+    }
+}
