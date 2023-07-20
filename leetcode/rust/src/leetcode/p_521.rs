9da66b037844007d8de6b2a21a074e3df738dacd
Ionut Lupu <lupu.ionut@protonmail.ch>
2023-07-20 16:20:56 +0300

    Solve 521. Longest Uncommon Subsequence I

---
+++ leetcode/rust/src/leetcode/p_521.rs
@@ -0,0 +1,11 @@
+// 521. Longest Uncommon Subsequence I
+// -----------------------------------
+
+impl Solution {
+    pub fn find_lu_slength(a: String, b: String) -> i32 {
+        if a == b {
+            return -1;
+        }
+        a.len().max(b.len()) as i32
+    }
+}
