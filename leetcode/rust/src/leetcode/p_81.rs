af5c64f9d15ad00b445e7d8435a98f6a414836bc
Ionut Lupu <lupu.ionut@protonmail.ch>
2023-08-10 16:06:23 +0300

    Solve 81. Search in Rotated Sorted Array II

---
+++ leetcode/rust/src/leetcode/p_81.rs
@@ -0,0 +1,34 @@
+// 81. Search in Rotated Sorted Array II
+// -------------------------------------
+
+impl Solution {
+    pub fn search(nums: Vec<i32>, target: i32) -> bool {
+        let mut l = 0;
+        let mut r = nums.len() - 1;
+
+        while l <= r {
+            let m = l + (r - l) / 2;
+
+            if nums[m] == target {
+                return true;
+            }
+
+            if nums[l] < nums[m] {
+                if nums[l] <= target && target < nums[m] {
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            } else if nums[l] > nums[m] {
+                if nums[m] < target && target <= nums[r] {
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
+            } else {
+                l += 1;
+            }
+        }
+        false
+    }
+}
