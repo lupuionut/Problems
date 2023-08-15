5961d9c172ccdc8fdbf2836382fe2ff0c1c85a6e
Ionut Lupu <lupu.ionut@protonmail.ch>
2023-08-15 17:16:39 +0300

    Solve 650. 2 Keys Keyboard

---
+++ leetcode/rust/src/leetcode/p_650.rs
@@ -0,0 +1,20 @@
+// 650. 2 Keys Keyboard
+// --------------------
+
+impl Solution {
+    pub fn min_steps(n: i32) -> i32 {
+        let mut dp = vec![0; (n + 1) as usize];
+        dp[1] = 0;
+
+        for i in 2..=n {
+            dp[i as usize] = i;
+            for j in 1..i {
+                if i % j == 0 {
+                    dp[i as usize] = dp[i as usize].min(dp[(i / j) as usize] + j);
+                }
+            }
+        }
+
+        dp[n as usize]
+    }
+}
