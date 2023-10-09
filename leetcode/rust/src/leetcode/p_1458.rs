ec6c5c953acd52c19ad1ba3a2171dfb7dfc35b7e
Ionut Lupu <lupu.ionut@protonmail.ch>
2023-10-09 14:31:20 +0300

    Solve 1458. Max Dot Product of Two Subsequences

--- leetcode/rust/src/leetcode/p_1458.rs
+++ leetcode/rust/src/leetcode/p_1458.rs
@@ -5,6 +5,19 @@ impl Solution {
     pub fn max_dot_product(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
         let mut cache = vec![vec![i32::MIN; nums2.len()]; nums1.len()];
 
+        let nums1_max = *nums1.iter().max().unwrap();
+        let nums1_min = *nums1.iter().min().unwrap();
+        let nums2_max = *nums2.iter().max().unwrap();
+        let nums2_min = *nums2.iter().min().unwrap();
+
+        if nums1_max < 0 && nums2_min > 0 {
+            return nums1_max * nums2_min;
+        }
+
+        if nums1_min > 0 && nums2_max < 0 {
+            return nums1_min * nums2_max;
+        }
+
         fn get_max(
             i1: usize,
             i2: usize,
@@ -12,19 +25,6 @@ impl Solution {
             nums2: &Vec<i32>,
             cache: &mut Vec<Vec<i32>>,
         ) -> i32 {
-            let nums1_max = *nums1.iter().max().unwrap();
-            let nums1_min = *nums1.iter().min().unwrap();
-            let nums2_max = *nums2.iter().max().unwrap();
-            let nums2_min = *nums2.iter().min().unwrap();
-
-            if nums1_max < 0 && nums2_min > 0 {
-                return nums1_max * nums2_min;
-            }
-
-            if nums1_min > 0 && nums2_max < 0 {
-                return nums1_min * nums2_max;
-            }
-
             if i1 == nums1.len() || i2 == nums2.len() {
                 return 0;
             }
