eb9c7d5dae2eff2a0d81ea3ef989edd5ea7cd13a
Ionut Lupu <lupu.ionut@protonmail.ch>
2023-11-17 09:40:20 +0200

    Solve 1877. Minimize Maximum Pair Sum in Array

---
+++ leetcode/rust/src/leetcode/p_1877.rs
@@ -0,0 +1,18 @@
+// 1877. Minimize Maximum Pair Sum in Array
+// ----------------------------------------
+
+impl Solution {
+    pub fn min_pair_sum(nums: Vec<i32>) -> i32 {
+        let mut nums = nums;
+        let mut ans = 0;
+        let n = nums.len();
+
+        nums.sort();
+
+        for i in 0..n / 2 {
+            ans = ans.max((nums[i] + nums[n - i - 1]));
+        }
+
+        ans
+    }
+}
