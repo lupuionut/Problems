bbc404893cdb7a8f88cba2efc6cf91360cc7a249
Ionut Lupu <lupu.ionut@protonmail.ch>
2024-10-14 08:16:46 +0300

    Solve 2530. Maximal Score After Applying K Operations

--- leetcode/rust/src/leetcode/p_2530.rs
+++ leetcode/rust/src/leetcode/p_2530.rs
@@ -4,31 +4,27 @@
 use std::collections::BinaryHeap;
 
 impl Solution {
-    pub fn max_kelements(nums: Vec<i32>, k: i32) -> i64 {
-        let mut heap: BinaryHeap<i32> = BinaryHeap::new();
-        let mut ans: i64 = 0;
-
-        fn ceil(n: i32, d: i32) -> i32 {
-            let mut r = n / d;
-            if n % d != 0 {
-                r += 1;
-            }
-            r
-        }
+    pub fn max_kelements(nums: Vec<i32>, mut k: i32) -> i64 {
+        let mut heap = BinaryHeap::new();
+        let mut ans = 0;
 
         nums.iter().for_each(|&n| {
-            heap.push(n);
+            heap.push(n as i64);
         });
 
-        let mut i = 0;
-
-        while i < k {
-            let n = heap.pop().unwrap();
-            ans += n as i64;
-            heap.push(ceil(n, 3));
-            i += 1;
+        while k > 0 {
+            if let Some(num) = heap.pop() {
+                ans += num;
+                let new = num / 3;
+                if num % 3 != 0 {
+                    heap.push(new + 1)
+                } else {
+                    heap.push(new);
+                }
+            }
+            k -= 1;
         }
-
         ans
     }
 }
+
