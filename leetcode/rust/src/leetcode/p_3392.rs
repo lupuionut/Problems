558fb54889a31f2dbcd91f15828f16518ce146fe
Ionut Lupu <lupu.ionut@protonmail.ch>
2025-04-27 07:10:52 +0300

    Solve 3392. Count Subarrays of Length Three With a Condition

---
+++ leetcode/rust/src/leetcode/p_3392.rs
@@ -0,0 +1,14 @@
+// 3392. Count Subarrays of Length Three With a Condition
+// ------------------------------------------------------
+
+impl Solution {
+    pub fn count_subarrays(nums: Vec<i32>) -> i32 {
+        let mut ans = 0;
+        for i in 1..nums.len() - 1 {
+            if (nums[i - 1] + nums[i + 1]) * 2 == nums[i] {
+                ans += 1;
+            }
+        }
+        ans
+    }
+}
