7c014f84d019f661f1b8d4d4fb83aab5783fa68b
Ionut Lupu <lupu.ionut@protonmail.ch>
2025-05-06 07:53:39 +0300

    Solve 1920. Build Array from Permutation

---
+++ leetcode/rust/src/leetcode/p_1920.rs
@@ -0,0 +1,12 @@
+// 1920. Build Array from Permutation
+// ----------------------------------
+
+impl Solution {
+    pub fn build_array(nums: Vec<i32>) -> Vec<i32> {
+        let mut ans = vec![0; nums.len()];
+        for i in 0..nums.len() {
+            ans[i] = nums[nums[i] as usize];
+        }
+        ans
+    }
+}
