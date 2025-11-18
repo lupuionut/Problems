87f12ac65020ce56db270c355f81185cae00d1af
ionut <lupu.ionut@protonmail.ch>
2025-11-18 08:01:26 +0200

    Solve 717. 1-bit and 2-bit Characters

---
+++ leetcode/rust/src/leetcode/p_717.rs
@@ -0,0 +1,20 @@
+// 717. 1-bit and 2-bit Characters
+// -------------------------------
+impl Solution {
+    pub fn is_one_bit_character(bits: Vec<i32>) -> bool {
+        let mut stack = 0;
+        for i in 0..bits.len() - 1 {
+            if stack == 1 {
+                stack = 0;
+            } else {
+                if bits[i] == 1 {
+                    stack = 1;
+                }
+            }
+        }
+        if stack == 1 {
+            return false;
+        }
+        true
+    }
+}
