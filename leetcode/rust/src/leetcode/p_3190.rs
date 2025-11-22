deff4da0c3d3550db5d3b3574f2059258306f0ef
ionut <lupu.ionut@protonmail.ch>
2025-11-22 08:45:11 +0200

    Solve 3190. Find Minimum Operations to Make All Elements Divisible by Three

---
+++ leetcode/rust/src/leetcode/p_3190.rs
@@ -0,0 +1,13 @@
+// 3190. Find Minimum Operations to Make All Elements Divisible by Three
+// ---------------------------------------------------------------------
+impl Solution {
+    pub fn minimum_operations(nums: Vec<i32>) -> i32 {
+        let mut ans = 0;
+        for num in nums {
+            if num % 3 != 0 {
+                ans += 1;
+            }
+        }
+        ans
+    }
+}
